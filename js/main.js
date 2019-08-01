5b45424f9f81bf77073b865aaff50db01a4a7283
Yusuf <Your Actual Email>
2019-08-01 15:11:33 +0200

    finished showcase structure and css

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function Function2() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function Function1() {
+    var x = document.getElementById("header");
+    if (x.className === "grid") {
+      x.className += " responsive";
+    } else {
+      x.className = "grid";
+    }
+  }
