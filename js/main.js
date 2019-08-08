3b6c80072c478e2c13d1e8e4d60ed283be35c0cc
Yusuf <Your Actual Email>
2019-08-08 12:09:03 +0200

    added main structure and css , and made responsive design

--- js/main.js
+++ js/main.js
@@ -58,3 +58,19 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "VIEW MORE"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "VIEW LESS"; 
+    moreText.style.display = "inline";
+  }
+}
